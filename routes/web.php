3040084d8245c90b3197f6081bdcf25380fea13c
Kyle Liu <kylesliu@outlook.com>
2018-05-05 17:33:32 +0800

    Add Index Page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/','Index\IndexController@index');
